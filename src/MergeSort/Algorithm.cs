46774f28cd90e0b3e61d69b152283bbd5ab3227b
oncicaradupopovici <oncicaradupopovici@gmail.com>
2017-01-21 13:11:22 +0200

    Add project files.

---
+++ src/MergeSort/Algorithm.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MergeSort
+{
+    public class Algorithm<T>
+        where T : IComparable<T>
+    {
+        public IEnumerable<T> MergeSort(IEnumerable<T> input)            
+        {
+            if (input.Count() <= 1)
+                return input;
+
+            var middleIndex = input.Count() / 2;
+            var left = MergeSort(input.Take(middleIndex)).ToArray();
+            var right = MergeSort(input.Skip(middleIndex)).ToArray();
+
+            return JoinSortedArrays(left, right);
+        }
+
+
+        private T[] JoinSortedArrays(T[] a, T[] b)
+        {
+            var result = new T[a.Length + b.Length];
+
+            var ai = 0;
+            var bi = 0;
+            var ri = 0;
+
+            var aIsDone = false;
+            var bIsDone = false;
+
+            while (!(aIsDone && bIsDone))
+            {
+                if(!aIsDone && (bIsDone || a[ai].CompareTo(b[bi]) < 0))
+                {
+                    result[ri] = a[ai];
+                    ai++;
+                    if (ai == a.Length)
+                        aIsDone = true;
+                }
+                else
+                {
+                    result[ri] = b[bi];
+                    bi++;
+                    if (bi == b.Length)
+                        bIsDone = true;
+                }
+                ri++;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ test/MergeSort.Test/MergeSortTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MergeSort.Test
+{
+    public class MergeSortTests
+    {
+        [Theory]
+        [InlineData(new[] { 3, 8, 2, 9, 6, 1, 7 })]
+        [InlineData(new[] { 5, 10, 45, 12, 1, 56, 25, 83, 98, 78, 35 })]
+        public void SampleTests(int[] input)
+        {
+            //Arrange
+            var sut = new MergeSort.Algorithm<int>();
+
+            //Act
+            var actual = sut.MergeSort(input);
+
+            //Assert
+            var expected = input.ToList();
+            expected.Sort();
+            Assert.Equal(expected, actual);
+        }
+    }
+}
